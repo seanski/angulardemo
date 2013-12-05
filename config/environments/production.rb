ef86e22117ded6a1f0ed592125b686e4f6690c24
Sean Hill <seandhi@gmail.com>
2013-12-05 12:23:43 -0600

    Do not mangle parameter names, allow slim files to be compiled in asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Angulardemo::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  config.assets.precompile += %w( *.ttf *.woff *.svg *.eot *.slim )
+  config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
